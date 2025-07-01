ff02d2b9d27302ac8aad6f0a04a7a22971d0e460
abhijit-ux <asdesigns360@gmail.com>
2025-07-01 10:52:31 +0000

    have fun

---
+++ operation.js
@@ -0,0 +1,13 @@
+let value =3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+
+
