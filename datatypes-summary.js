cf3f865ee605bf14deca371399a063817e074391
abhijit-ux <asdesigns360@gmail.com>
2025-07-21 14:23:04 +0000

    Datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,34 @@
+// primitive
+
+// 7 types : String , Number ,Boolean, Null ,Undefined ,Symbol, BigInt
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggedIn =false
+const outsideTemp =null
+let userEmail;
+
+const id =Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 12124535645676578686678n
+
+// Non-Primitive
+
+// Array , Object ,Functions
+
+const heros = ["luffy" , "Ace" , "zoro"];
+
+let myObject ={
+    name: "Abhi",
+    age: 19,
+}
+
+const myFunction =function(){
+    console.log("hello World")
+}
+
+console.log(typeof id);
