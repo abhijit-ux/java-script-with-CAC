bd8caf72bd9ba2937aefbab00b4aee4d026d3c60
abhijit-ux <asdesigns360@gmail.com>
2025-07-21 13:56:05 +0000

    conparison

--- operation.js
+++ operation.js
@@ -1,13 +1,16 @@
-let value =3
-let negValue = -value
-console.log(negValue);
+// let value =3
+// let negValue = -value
+// console.log(negValue);
 
-console.log(2+2);
-console.log(2-2);
-console.log(2*2);
-console.log(2**2);
-console.log(2/2);
-console.log(2%2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
 
-
+let str1 ="hello"
+let str2 = " Abhi"
+let str3 = str1 +str2
+console.log(str3);
 
